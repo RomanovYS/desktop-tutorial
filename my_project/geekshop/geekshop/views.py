3d6dd38a257f1a9ed54060d0f0b4a388672f2ee4
Yuriy <heone@mail.ru>
2021-06-29 07:15:06 +0300

    HW2

---
+++ my_project/geekshop/geekshop/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+def index(request):
+    title = 'магазин'
+    context = {
+        'title': title
+    }
+    return render(request, 'index.html', context=context)
+
+
+def contacts(request):
+    title = 'контакты'
+    context = {
+        'title': title
+    }
+    return render(request, 'contact.html', context=context)
+
+
+# def pathpage(request):
+#     urls = request.META.PATH_INFO
+#     return urls
