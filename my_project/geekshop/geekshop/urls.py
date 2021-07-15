9950413aa22c277b21a575b6b686d6065ab26c00
Yuriy <heone@mail.ru>
2021-07-15 21:00:15 +0300

    WH7

---
+++ my_project/geekshop/geekshop/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import index, contacts
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('admin_staff/', include('adminapp.urls', namespace='admin_staff')),
+    path('', index, name="index"),
+    path('contacts/', contacts, name="contacts"),
+    path('products/', include('mainapp.urls', namespace='products')),
+    path('basket/', include('basketapp.urls', namespace='basket')),
+    path('auth/', include('authapp.urls', namespace='auth')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
