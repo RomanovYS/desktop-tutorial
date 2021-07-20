e2c0fde24f8eff8ce5a04729e4a68f2e308bf597
Yuriy <heone@mail.ru>
2021-07-20 18:46:33 +0300

    WH8

---
+++ my_project/geekshop/geekshop/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from basketapp.models import Basket
+from mainapp.models import Product
+
+
+def index(request):
+    title = 'магазин'
+    products = Product.objects.all()[:3]
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
+    context = {
+        'title': title,
+        'products': products,
+        'basket': basket,
+    }
+    return render(request, 'index.html', context=context)
+
+
+def contacts(request):
+    title = 'контакты'
+    context = {
+        'title': title
+    }
+    return render(request, 'contact.html', context=context)
